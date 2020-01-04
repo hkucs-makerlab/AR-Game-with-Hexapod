0f61a709cdd2530de38ae78e2d020aa584982dc8
Chun Sin Ying <chunsinying1234567890@gmail.com>
2020-01-05 03:10:20 +0800

    Control movement of hexapod

--- Controller/app/src/main/java/com/hkucsfyp/controller/ControllerService.java
+++ Controller/app/src/main/java/com/hkucsfyp/controller/ControllerService.java
@@ -3,10 +3,9 @@ package com.hkucsfyp.controller;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothSocket;
-import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -16,13 +15,80 @@ import java.util.UUID;
 public class ControllerService {
     private static final String TAG = "ControllerServiceLog";
 
-    public static final int ENABLE_BLUETOOTH = 1;
-    private static final UUID hexapodUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private static ControllerService instance;
-
     private Activity activity;
+
+    // Bluetooth
+    public static final int ENABLE_BLUETOOTH = 1;
+    private static final UUID HEXAPOD_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private BluetoothAdapter bluetoothAdapter;
-    private BluetoothSocket bluetoothSocket = null;
+    private BluetoothDevice bluetoothDevice;
+    private BluetoothSocket bluetoothSocket;
+    private boolean connected;
+
+    // Command Handler
+    private static final int REGULAR_TIME_INTERVAL = 1000;
+    private Handler regularHandler;
+
+    // Command Format
+    private static final char HEADER = 'V';
+    private static final char VERSION = '1';
+    private static final int LENGTH = 8;
+    public enum MODE_LETTER {
+        W('W'),
+        D('D'),
+        F('F');
+
+        private char value;
+
+        MODE_LETTER(char value) {
+            this.value = value;
+        }
+
+        public char getValue() {
+            return value;
+        }
+    }
+    public enum MODE_NUMBER {
+        ONE('1'),
+        TWO('2'),
+        THREE('3'),
+        FOUR('4');
+
+        private char value;
+
+        MODE_NUMBER(char value) {
+            this.value = value;
+        }
+
+        public char getValue() {
+            return value;
+        }
+    }
+    public enum DPAD_LETTER {
+        s('s'),
+        f('f'),
+        b('b'),
+        l('l'),
+        r('r'),
+        w('w');
+
+        private char value;
+
+        DPAD_LETTER(char value) {
+            this.value = value;
+        }
+
+        public char getValue() {
+            return value;
+        }
+    }
+    private static final char BEEP = 'B';
+
+    private MODE_LETTER modeLetter;
+    private MODE_NUMBER modeNumber;
+    private DPAD_LETTER dPADLetter;
+    private int beepFrequency, beepDuration;
 
     public static ControllerService createInstance(Activity activity) {
         if (instance == null) {
@@ -33,9 +99,21 @@ public class ControllerService {
 
     private ControllerService(Activity activity) {
         this.activity = activity;
+
+        connected = false;
+        initBluetooth();
+
+        modeLetter = MODE_LETTER.W;
+        modeNumber = MODE_NUMBER.ONE;
+        dPADLetter = DPAD_LETTER.s;
+        beepFrequency = 0;
+        beepDuration = 0;
+
+        regularHandler = new Handler();
+        regularHandler.postDelayed(sendMessage, REGULAR_TIME_INTERVAL);
     }
 
-    public void initBluetooth() {
+    private void initBluetooth() {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdapter == null) {
             Toast.makeText(activity, "Bluetooth is not supported", Toast.LENGTH_LONG).show();
@@ -48,42 +126,81 @@ public class ControllerService {
     }
 
     public void connectHexapod() {
-        try {
-            BluetoothDevice bluetoothDevice = bluetoothAdapter.getRemoteDevice("AB:90:78:56:50:9D");
-            bluetoothSocket = bluetoothDevice.createInsecureRfcommSocketToServiceRecord(hexapodUUID);
-            bluetoothAdapter.cancelDiscovery();
-            bluetoothSocket.connect();
-        } catch (Exception e) {
+        if (!connected) {
+            try {
+                bluetoothDevice = bluetoothAdapter.getRemoteDevice("AB:90:78:56:50:9D");
+                bluetoothSocket = bluetoothDevice.createInsecureRfcommSocketToServiceRecord(HEXAPOD_UUID);
+                bluetoothAdapter.cancelDiscovery();
+                bluetoothSocket.connect();
+                connected = true;
+                Toast.makeText(activity, "Connection succeed", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Toast.makeText(activity, "Connection failed", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(activity, "Connected", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void sd() {
-        try {
-            bluetoothSocket.getOutputStream().write(formatMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
+    private Runnable sendMessage = new Runnable() {
+        @Override
+        public void run() {
+            if (connected) {
+                try {
+                    bluetoothSocket.getOutputStream().write(formatMessage());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            regularHandler.postDelayed(this, REGULAR_TIME_INTERVAL);
         }
-    }
+    };
 
     private byte[] formatMessage() {
-        int input = 100;
-        int high=(input>>8) & 0xff;
-        int low=input & 0xff;
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
-        outputStream.write((byte) 'V');
-        outputStream.write((byte) '1');
-        outputStream.write((byte) 8);
-        outputStream.write((byte) 'W');
-        outputStream.write((byte) '1');
-        outputStream.write((byte) 'r');
-        outputStream.write((byte) 'B');
-        outputStream.write((byte) high);
-        outputStream.write((byte) low);
-        outputStream.write((byte) high);
-        outputStream.write((byte) low);
-        int checksum = high + low + high + low + 8 + 'W' + '1' + 'r' + 'B';
-        checksum = (checksum % 256);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        outputStream.write((byte) HEADER);
+        outputStream.write((byte) VERSION);
+        outputStream.write((byte) LENGTH);
+        outputStream.write((byte) modeLetter.getValue());
+        outputStream.write((byte) modeNumber.getValue());
+        outputStream.write((byte) dPADLetter.getValue());
+        outputStream.write((byte) BEEP);
+
+        int highFreq = (beepFrequency>>8) & 0xff;
+        int lowFreq = beepFrequency & 0xff;
+        outputStream.write((byte) highFreq);
+        outputStream.write((byte) lowFreq);
+
+        int highDur = (beepDuration>>8) & 0xff;
+        int lowDur = beepDuration & 0xff;
+        outputStream.write((byte) highDur);
+        outputStream.write((byte) lowDur);
+
+        int checksum = LENGTH + modeLetter.getValue() + modeNumber.getValue() + dPADLetter.getValue() + BEEP + highFreq + lowFreq + highDur + lowDur;
+        checksum = checksum % 256;
         outputStream.write((byte) ((char)checksum));
+
         return outputStream.toByteArray();
+    }
+
+    public void setModeLetter(MODE_LETTER modeLetter) {
+        this.modeLetter = modeLetter;
+    }
+
+    public void setModeNumber(MODE_NUMBER modeNumber) {
+        this.modeNumber = modeNumber;
+    }
+
+    public void setdPADLetter(DPAD_LETTER dPADLetter) {
+        this.dPADLetter = dPADLetter;
+    }
+
+    public void setBeepFrequency(int beepFrequency) {
+        this.beepFrequency = beepFrequency;
+    }
+
+    public void setBeepDuration(int beepDuration) {
+        this.beepDuration = beepDuration;
     }
 }
--- Controller/app/src/main/java/com/hkucsfyp/controller/MainActivity.java
+++ Controller/app/src/main/java/com/hkucsfyp/controller/MainActivity.java
@@ -2,16 +2,20 @@ package com.hkucsfyp.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivityLog";
+
     private ControllerService controllerService;
 
     @Override
@@ -21,10 +25,24 @@ public class MainActivity extends AppCompatActivity {
 
         controllerService = ControllerService.createInstance(MainActivity.this);
 
-        Button btnConnect = findViewById(R.id.button);
-        btnConnect.setOnClickListener(v -> controllerService.initBluetooth());
-        Button btnSd = findViewById(R.id.button2);
-        btnSd.setOnClickListener(v -> controllerService.sd());
+        Button btnConnect = findViewById(R.id.btn_connect);
+        btnConnect.setOnClickListener(v -> controllerService.connectHexapod());
+
+        for (ControllerService.MODE_LETTER letter : ControllerService.MODE_LETTER.values()) {
+            for (ControllerService.MODE_NUMBER number : ControllerService.MODE_NUMBER.values()) {
+                int resourceId = MainActivity.this.getResources().getIdentifier("button_id_" + letter.getValue() + number.getValue(), "id", MainActivity.this.getPackageName());
+                ImageButton button = findViewById(resourceId);
+                setModeListener(button, letter, number);
+            }
+        }
+
+        for (ControllerService.DPAD_LETTER letter : ControllerService.DPAD_LETTER.values()) {
+            if (letter != ControllerService.DPAD_LETTER.s) {
+                int resourceId = MainActivity.this.getResources().getIdentifier("button_id_" + letter.getValue(), "id", MainActivity.this.getPackageName());
+                    ImageButton button = findViewById(resourceId);
+                    setDPadListener(button, letter);
+            }
+        }
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -32,5 +50,28 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 controllerService.connectHexapod();
             }
+    }
+
+    void setModeListener(ImageButton button, ControllerService.MODE_LETTER letter, ControllerService.MODE_NUMBER number) {
+        button.setOnClickListener(v -> {
+            controllerService.setModeLetter(letter);
+            controllerService.setModeNumber(number);
+        });
+    }
+
+    void setDPadListener(ImageButton button, ControllerService.DPAD_LETTER letter) {
+        button.setOnTouchListener((v, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    controllerService.setdPADLetter(letter);
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    controllerService.setdPADLetter(ControllerService.DPAD_LETTER.s);
+                    break;
+            }
+            v.performClick();
+            return false;
+        });
     }
 }
