cccd07244077842771d13ba9f0b5a6b8e8cc6b4a
Chun Sin Ying <chunsinying1234567890@gmail.com>
2020-04-22 02:17:54 +0800

    Fighting system 2

---
+++ Hexplore Fort/Assets/HexploreFort/Scripts/VirtualPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HF_Static;
+
+public class VirtualPlayer : MonoBehaviour
+{
+    public GameObject skill, defendShield;
+    private Animator animator;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    public IEnumerator Shoot() {
+        animator.SetBool("isShooting", true);
+
+        yield return new WaitForSeconds(StaticData.FIGHT_OPERATION_TIME);
+
+        animator.SetBool("isShooting", false);
+    }
+
+    public IEnumerator ActivateSkill() {
+        skill.SetActive(true);
+
+        yield return new WaitForSeconds(StaticData.FIGHT_OPERATION_TIME);
+
+        skill.SetActive(false);
+    }
+
+    public IEnumerator Defend() {
+        defendShield.SetActive(true);
+
+        yield return new WaitForSeconds(StaticData.FIGHT_OPERATION_TIME);
+
+        defendShield.SetActive(false);
+    }
+
+    public void EndFight() {
+        animator.SetBool("isShooting", false);
+        skill.SetActive(false);
+        defendShield.SetActive(false);
+    }
+}
